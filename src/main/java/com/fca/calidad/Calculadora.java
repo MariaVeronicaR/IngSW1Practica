e3c20fbed37975766dac8af55b80cee6bb9fd513
MariaVeronicaR <mvrm10@gmail.com>
2020-09-06 15:56:59 -0500

    copiando linea

--- src/main/java/com/fca/calidad/Calculadora.java
+++ src/main/java/com/fca/calidad/Calculadora.java
@@ -5,6 +5,9 @@ public class Calculadora{
     
   public  float divide(int operador1, int operador2) {
         resultado=(operador1/operador2);
+
+        System.out.println("La division de "+operador1+" / "+operador2+" es "+resultado);
+
         System.out.println("La division de "+operador1+" / "+operador2+" es "+resultado);
         return resultado;
     }
